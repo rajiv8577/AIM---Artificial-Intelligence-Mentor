ebca0801ef5ffebb1c8a19e9348498255dd4b8a9
Rajiv Malviya <rajivmalviya8577@gmail.com>
2026-01-03 16:24:13 +0530

    fixed background

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-
     <div>
-      
+      <div className="grid-background"></div>
     </div>
   );
 }
