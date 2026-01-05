e2de42f774abcdccb2b7c1ed1f3efee401a77f1e
Rajiv Malviya <rajivmalviya8577@gmail.com>
2026-01-05 23:57:23 +0530

    compledted HeroSection and added datas

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
     <div>
       <div className="grid-background"></div>
+      
+      <HeroSection />
     </div>
   );
 }
